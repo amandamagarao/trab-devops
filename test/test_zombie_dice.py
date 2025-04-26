179d086836c8f06413bc277e100ea4bd48827f7a
Amanda Kirsten Magarão Souza <99298361+amandamagarao@users.noreply.github.com>
2025-04-26 07:32:22 -0300

    Update test_zombie_dice.py

--- test/test_zombie_dice.py
+++ test/test_zombie_dice.py
@@ -1,11 +1,14 @@
 import sys
 import os
 import pytest
+from unittest.mock import patch
 
 # Adiciona o caminho do src ao sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
-from src.main import resetarcopo
+# Usando patch para evitar que o input() do main.py quebre o teste
+with patch('builtins.input', return_value='2'):
+    from src.main import resetarcopo
 
 def test_resetar_copo():
     dados = resetarcopo()
@@ -13,3 +16,4 @@ def test_resetar_copo():
     assert dados.count('dVerde') == 6
     assert dados.count('dAmarelo') == 4
     assert dados.count('dVermelho') == 3
+
