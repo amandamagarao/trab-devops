0c9548fac52469eb083bab7cdfc78579903f0aa6
[amandamagarao] <99298361+amandamagarao@users.noreply.github.com>
2025-04-26 12:00:53 +0200

    implementacao

--- test/test.py
+++ test/test.py
@@ -1,2 +1,15 @@
 import sys
 import os
+import pytest
+
+# Adiciona o caminho do src ao sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+from src.main import resetarcopo
+
+def test_resetar_copo():
+    dados = resetarcopo()
+    assert len(dados) == 13
+    assert dados.count('dVerde') == 6
+    assert dados.count('dAmarelo') == 4
+    assert dados.count('dVermelho') == 3
